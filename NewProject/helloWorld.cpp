0d96c260a72d472daa60d2e6083ba1a968bc73e6
MinseoChoi <201802166@o.cnu.ac.kr>
2019-09-09 11:43:28 +0900

    초기 커밋.

---
+++ NewProject/helloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class LetDebug {
+public:
+	void printNum() {
+		short s1 = 32767;
+		short s2 = 1;
+		int s3 = (int)s1 + s2;
+		std::cout << s3 << std::endl;
+	}
+};
+int main() {
+	LetDebug *ld = new LetDebug; // 클래스 객체화
+	ld->printNum();
+
+	return 0;
+}
